c90a95a3b63c55a7746a543f4987d4ad4d46e0ee
Soham Chowdhury <chow.soham@gmail.com>
2017-12-22 17:11:27 +0530

    Add shell.nix template

---
+++ shell.template.nix
@@ -0,0 +1,33 @@
+let 
+  rien = import ./rien.nix {
+    packageName = "hello-world";
+    packagePath = ./.;
+
+    # Instead of using <nixpkgs>, use a lock-file to stick to
+    # a particular `nixpkgs` commit.
+    nixpkgsLock = ./nixpkgs.json;
+
+    ghcVersion = "ghc822";
+  };
+
+in
+  rien.shell {
+    # Generate Hoogle documentation?
+    wantHoogle = true;
+
+    # Haskell dependencies
+    deps = hsPkgs: with hsPkgs; [
+      # mtl
+    ];
+
+    # Optionally, also add sets of related packages that are
+    # commonly used together.
+    depSets = hsPkgs: with (rien.package-sets hsPkgs); [
+      # conduitFull
+    ];
+
+    # Native dependencies
+    nativeDeps = pkgs: with pkgs; [
+      # llvm
+    ];
+  }
